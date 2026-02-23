42c3eabc819d937acc1ee8ce5b214ad9799f3a0c
Shreenithi-C <shreenithii3003@gmail.com>
2026-02-23 16:23:35 +0530

    Coded to Store and retrieve game results for future reference.

--- Guessing_App.java
+++ Guessing_App.java
@@ -1,15 +1,25 @@
 /* 
 @author developer
-@version 4
+@version 5
 */
 import java.util.*;
 public class Guessing_App {
 	public static void main(String[] args) throws InvalidInputException{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("===========================");
 		System.out.println("Welcome to the Guessing App");
+		System.out.println("===========================");
+
+		System.out.print("Enter PLayer Name: ");
+		String player=sc.nextLine();
+
 		GameConfig config=new GameConfig();
 		config.showRules();
-		Scanner sc=new Scanner(System.in);
+		
 		int attempts=0;
+		int hintsUsed=0;
+		boolean win=false;
+
 		while(attempts<config.getMaxAttempts())
 		{
 			System.out.print("Enter your guess:");
@@ -25,7 +35,7 @@ public class Guessing_App {
      			if("CORRECT".equals(result)){
 				break;
 			}
-
 		}
-	}
+		StorageService.saveResult(player, attempts, win);
+	}		
 }
