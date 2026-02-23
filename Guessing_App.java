0826223b4ad0bb046dcb09d489705ffa7cf83f0a
Shreenithi-C <shreenithii3003@gmail.com>
2026-02-23 14:52:33 +0530

    Coded for Error Handling and Validation

--- Guessing_App.java
+++ Guessing_App.java
@@ -1,10 +1,10 @@
 /* 
 @author developer
-@version 2
+@version 4
 */
 import java.util.*;
 public class Guessing_App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InvalidInputException{
 		System.out.println("Welcome to the Guessing App");
 		GameConfig config=new GameConfig();
 		config.showRules();
@@ -13,12 +13,19 @@ public class Guessing_App {
 		while(attempts<config.getMaxAttempts())
 		{
 			System.out.print("Enter your guess:");
-			int guess = sc.nextInt();
+			int guess = ValidationService.validateInput(sc.nextLine());
 			attempts++;
 			String result=GuessValidator.validateGuess(guess, config.getTargetNumber());
 			System.out.println(result);
-			if("CORRECT".equals(result)){
+			if("CORRECT".equals(result) && hintsUsed<config.getMAxHints()){
+				hintsUsed++;
+				System.out.println(HintService.generateHint(config.getTargetNumber(),hintsUsed);
+			}
+			System.out.println(result);
+     			if("CORRECT".equals(result)){
 				break;
 			}
+
+		}
 	}
 }
