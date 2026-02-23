ca0a638bf6480cf1d527a33ab9f4eef6b266d0e5
Shreenithi-C <shreenithii3003@gmail.com>
2026-02-23 14:10:57 +0530

    Coded user guess submission

---
+++ GuessValidator.java
@@ -0,0 +1,13 @@
+class GuessValidator
+{
+	public static String validateGuess(int guess,int target)
+	{
+		if(guess==target){
+			return "CORRECT";
+		}
+		else if(guess < target){
+			return "LOW";
+		}
+		return "HIGH";
+	}
+}
