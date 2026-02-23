cfe80cc72252b6a814611fd050f60319d48fcdbc
Shreenithi-C <shreenithii3003@gmail.com>
2026-02-23 14:13:20 +0530

    Coded to accept user guesses

--- Guessing_App.java
+++ Guessing_App.java
@@ -1,12 +1,24 @@
 /* 
 @author developer
-@version 1
+@version 2
 */
 import java.util.*;
 public class Guessing_App {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Guessing App");
-		GameConfig gameConfig=new GameConfig();
-		gameConfig.showRules();
+		GameConfig config=new GameConfig();
+		config.showRules();
+		Scanner sc=new Scanner(System.in);
+		int attempts=0;
+		while(attempts<config.getMaxAttempts())
+		{
+			System.out.print("Enter your guess:");
+			int guess = sc.nextInt();
+			attempts++;
+			String result=GuessValidator.validateGuess(guess, config.getTargetNumber());
+			System.out.println(result);
+			if("CORRECT".equals(result)){
+				break;
+			}
 	}
 }
