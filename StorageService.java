3834432164a9c2cf1bb8df8642bb784d4f9768f6
Shreenithi-C <shreenithii3003@gmail.com>
2026-02-23 16:16:28 +0530

    Coded to save the final outcome of the game

---
+++ StorageService.java
@@ -0,0 +1,12 @@
+class StorageService{
+	public static void saveResult(String player,int attempts,boolean win)
+	{
+		try(BufferedWriter writer=new BufferedWriter(new FileWriter("game_results.txt",true))){
+			writer.write("Player: "+player+", Attempts: "+attempts +", Result: "+(win ?"WIN":"LOSE"));
+			writer.newLine();
+		}
+		catch(IOException e){
+			System.out.println("Unable to save game result.");
+		}
+	}
+}
